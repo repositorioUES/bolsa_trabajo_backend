074c51d664794582d3bc3f64041ac07340e4e50d
Christian Garcia <kriz10@outlook.com>
2023-05-21 18:07:08 -0600

    inicio del proyecto

---
+++ src/main/java/com/bad115/SistemaBolsa/BolsaTrabajoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bad115.SistemaBolsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaTrabajoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsaTrabajoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bad115/SistemaBolsa/BolsaTrabajoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bad115.SistemaBolsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaTrabajoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
