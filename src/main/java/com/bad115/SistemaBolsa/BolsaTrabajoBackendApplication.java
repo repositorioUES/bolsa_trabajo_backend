4f162140d17e5454c1eeaac0f81a4b794f657df8
Christian Garcia <kriz10@outlook.com>
2023-05-27 17:41:03 -0600

    Agregando seguridad para credenciales DB

--- src/main/java/com/bad115/SistemaBolsa/BolsaTrabajoBackendApplication.java
+++ src/main/java/com/bad115/SistemaBolsa/BolsaTrabajoBackendApplication.java
@@ -2,12 +2,19 @@ package com.bad115.SistemaBolsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BolsaTrabajoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BolsaTrabajoBackendApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String apiConfirmacion(){
+		return "La api esta corriendo ";
+	}
 }
